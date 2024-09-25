f25a25b5a507243e2b20aa697db155f82b1639bc
NeiraJ <johnny.neira@gmail.com>
2024-09-25 07:56:51 +0200

    Amélioration de la configuration de l'application
    - Ajout des imports pour les blueprints 'auth' et 'blog'
    - Enregistrement des blueprints 'auth' et 'blog' dans l'application
    - Correction de l'indentation et suppression de la duplication de l'appel à initialize_database_in_threads()
    - Amélioration de la lisibilité du retour de la fonction home()

--- pg_app/__init__.py
+++ pg_app/__init__.py
@@ -4,15 +4,21 @@ from pg_app.src.utils.factories import initialize_database_in_threads
 from pg_app.src.models.livre import Livre
 from pg_app.src.models.editeur import Editeur
 
+from . import auth
+from . import blog
+
 
 def create_app():
     app = Flask(__name__)
+
+    initialize_database_in_threads()
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
 
     @app.route("/")
     def home():
         list_of_selected_books = Livre.book_list
         list_of_editors = Editeur.editor_list
-        return render_template('index.html', livres=list_of_selected_books, editors = list_of_editors)
+        return render_template('index.html', livres=list_of_selected_books, editors=list_of_editors)
 
-    initialize_database_in_threads()
     return app

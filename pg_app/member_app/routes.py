11b40a259fb42e5ca871596015a0309dd1584d38
NeiraJ <johnny.neira@gmail.com>
2024-09-27 17:08:18 +0200

    REFACTORING

--- pg_app/member_app/routes.py
+++ pg_app/member_app/routes.py
@@ -11,6 +11,8 @@ from pg_app.src.dao.appartient_dao import AppartientDAO
 from . import bp
 from .froms import RoundvoteRadioForm, DefineWinnerForm
 
+TEMPLATE_MEMBER_INDEX = 'member/index.html'
+
 
 @bp.route('/president-menu', methods=['POST', 'GET'])
 @login_required
@@ -22,23 +24,21 @@ def index():
     :return: Le template du menu du président
     """
     dao_appartient: AppartientDAO = AppartientDAO()
-    liste_originale: list[int] = dao_appartient.query_database(
+    liste_originale: list[dict] = dao_appartient.query_database(
         "SELECT id_livre FROM `appartient` WHERE `id_selection` = 3")
     liste_id = [livre['id_livre'] for livre in liste_originale]
     actual_round: int = dao_appartient.get_current_round()
     form = RoundvoteRadioForm()
     form1 = DefineWinnerForm(liste_id) if actual_round == 3 else None
-    html = 'member/index.html'
 
     if form.validate_on_submit():
         round_vote = form.round_vote.data
         length_of_selection: int = 8 if round_vote == '2' else 4
         vote_results: list[dict] = render_votes_results(int(round_vote), length_of_selection)
-        print(vote_results)
-        return render_template('%s' % html, results=vote_results, form=form, form1=form1,
+        return render_template('%s' % TEMPLATE_MEMBER_INDEX, results=vote_results, form=form, form1=form1,
                                actual_round=actual_round)
 
-    return render_template(html, form=form, form1=form1, actual_round=actual_round)
+    return render_template(TEMPLATE_MEMBER_INDEX, form=form, form1=form1, actual_round=actual_round)
 
 
 @bp.route('/president-menu/confirmation', methods=['POST'])
@@ -47,17 +47,10 @@ def confirmation():
     """Confirme les résultats affichés"""
     if request.method == 'POST':
         try:
-            confirmation = request.form.get('confirmation')
+            confirmation_form: str | None = request.form.get('confirmation')
 
-            if confirmation is not None:
-                id_livres = []
-                # Récupérer tous les clés de la MultiDict
-                for key in request.form.keys():
-                    # Vérifier si la clé correspond à un id_livre
-                    if key.startswith('results[') and 'id_livre' in key:
-                        # Extraire l'id_livre et l'ajouter à la liste
-                        id_livre = request.form[key]
-                        id_livres.append(int(id_livre))
+            if confirmation_form is not None:
+                id_livres = get_ids_from_request()
                 if confirm_vote(id_livres):
                     flash("Les résultats ont été confirmés et enregistrés avec succès.", "success")
                 else:
@@ -66,15 +59,28 @@ def confirmation():
                 return redirect(url_for('president-menu.index'))
         except UnicodeDecodeError:
             return "Erreur d'encodage dans les résultats soumis", 400
-    return render_template('member/index.html')
+    return render_template(TEMPLATE_MEMBER_INDEX)
 
 
-@bp.route('/president-menu/define-winner', methods=['POST'])
-def define_winner():
-    form1 = DefineWinnerForm()
+def get_ids_from_request():
+    """Récupère les id_livres soumis dans le formulaire"""
+    id_livres = []
+    # Récupérer tous les clés de la MultiDict
+    for key in request.form.keys():
+        # Vérifier si la clé correspond à un id_livre
+        if key.startswith('results[') and 'id_livre' in key:
+            # Extraire l'id_livre et l'ajouter à la liste
+            id_livre = request.form[key]
+            id_livres.append(int(id_livre))
+    return id_livres
 
-    if form1.validate_on_submit():
-        winner_id = form1.winner.data
-        print(winner_id)
 
-    return render_template('member/index.html')
+# @bp.route('/president-menu/define-winner', methods=['POST'])
+# def define_winner():
+#     form1 = DefineWinnerForm()
+#
+#     if form1.validate_on_submit():
+#         winner_id = form1.winner.data
+#         print(winner_id)
+#
+#     return render_template(TEMPLATE_MEMBER_INDEX)

5955b9c0054294b953a448ef4b403c9f7b9ad233
NeiraJ <johnny.neira@gmail.com>
2024-09-27 16:49:32 +0200

    Ajout d'un nouveau formulaire pour définir le gagnant
    - Implémentation de la classe DefineWinnerForm
    - Ajout d'un champ SelectField pour choisir le gagnant

--- pg_app/member_app/froms.py
+++ pg_app/member_app/froms.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, SubmitField
+from wtforms import RadioField, SelectField, SubmitField
 
 
 class RoundvoteRadioForm(FlaskForm):
+    """Formulaire pour la tour de vote."""
+
     round_vote = RadioField("Ronde de vote", choices=[(2, "Tour 2"), (3, "Tour 3")])
     submit = SubmitField("Voir les resultats")
+
+class DefineWinnerForm(FlaskForm):
+    def __init__(self, list_choices=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if list_choices:
+            self.winner.choices = list_choices
+
+    """Formulaire pour définir le gagnant."""
+    winner = SelectField('Gagnant', choices=[])
+    submit = SubmitField('Confirmer')

67effcf822d9cdc9d7ac85c1e0f8fb746ee6d98f
NeiraJ <johnny.neira@gmail.com>
2024-11-26 08:46:33 +0100

    feat : Implémenter les fonctions utilitaires pour gérer les livres
    - Créer la fonction book_to_dict pour convertir les objets Livre en dictionnaires structurés
    - Implémenter la fonction find_book_by pour rechercher des livres par attribut spécifique
    - Importer la classe Livre depuis pg_app
    - Utiliser des annotations de type et des docstrings pour améliorer la lisibilité du code
    - Structurer les fonctions pour faciliter leur réutilisation dans l'API

---
+++ pg_app/api_app/utils.py
@@ -0,0 +1,21 @@
+
+from pg_app import Livre
+
+
+def book_to_dict(book) -> dict:
+    """Convertit un objet Livre en un dictionnaire structuré."""
+    data = {book.title: {
+        "id": book.id,
+        "summary": book.summary,
+        "publication_date": book.publication_date,
+        "number_of_pages": book.number_of_pages,
+        "isbn": book.isbn,
+        "price": book.price,
+    }}
+    return data
+
+
+def find_book_by(attr, value):
+    """Recherche un livre dans la liste des livres en fonction
+     d'un attribut spécifique et de sa valeur."""
+    return next((book for book in Livre.book_list if getattr(book, attr) == value), None)
